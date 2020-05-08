2cb245de477cca77d0983b2755e75872238113db
alokKumarSingh89 <itsaloksigh2011@gmail.com>
2020-05-08 16:18:42 +0800

    Updates

---
+++ precache-manifest.84946359b8dd10908bb91d25b2ee8fb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e737edce102e068ac51537728531f8",
+    "url": "/react-showcase-project/index.html"
+  },
+  {
+    "revision": "ea032f91ede9cbe568e3",
+    "url": "/react-showcase-project/static/css/main.e87020a3.chunk.css"
+  },
+  {
+    "revision": "09137ac39c5b25fcfa91",
+    "url": "/react-showcase-project/static/js/2.bb274b18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-showcase-project/static/js/2.bb274b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea032f91ede9cbe568e3",
+    "url": "/react-showcase-project/static/js/main.8f5015fa.chunk.js"
+  },
+  {
+    "revision": "43a84ad495dfd405e9c6",
+    "url": "/react-showcase-project/static/js/runtime-main.c6d03013.js"
+  },
+  {
+    "revision": "db32600a9d03746fbe493da5e2821989",
+    "url": "/react-showcase-project/static/media/photo.db32600a.jpg"
+  }
+]);
